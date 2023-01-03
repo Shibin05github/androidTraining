df54c803fe47546f0602f521d7305ed63dd69f70
Shibin Antony <shibin.a@saasvaap.com>
2023-01-03 12:32:50 +0530

    android training code

--- app/src/main/java/com/helloworld/DatePicker_Activity.java
+++ app/src/main/java/com/helloworld/DatePicker_Activity.java
@@ -8,17 +8,28 @@ import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
+import com.helloworld.Utils.NetworkUtils;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
@@ -26,10 +37,13 @@ public class DatePicker_Activity extends AppCompatActivity {
 
     private TextView tvSelectDate;
     private TextView tvSelectTime;
+    private TextView tvShowResult;
     private Dialog dialog;
     private Button btnShowSnack;
+    private Button btnAPI_Call;
     private RelativeLayout rl ;
-
+    private EditText etCityName;
+    String cityName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -97,6 +111,73 @@ public class DatePicker_Activity extends AppCompatActivity {
                     }).setActionTextColor(Color.BLACK)
                     .show();
         });
+
+        btnAPI_Call = findViewById(R.id.btnCallAPI);
+        etCityName = findViewById(R.id.etText);
+        tvShowResult = findViewById(R.id.tvResult);
+        btnAPI_Call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (view.getId() == R.id.btnCallAPI){
+                     cityName = etCityName.getText().toString();
+                    try {
+                        getData();
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+}
+
+    public void getData() throws MalformedURLException {
+        Uri uri = Uri.parse("https://datausa.io/api/data?drilldowns=State&measures=Population&year=latest").buildUpon().build();
+        URL url = new URL(uri.toString());
+        new DoTask().execute(url);
+    }
+    class DoTask extends AsyncTask<URL,Void,String>{
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            URL url = urls[0];
+            String data =null;
+            try {
+                data = NetworkUtils.makeHTTPRequest(url);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            try {
+                parseJson(s);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        public void parseJson(String data) throws JSONException {
+            JSONObject jsonObject = null;
+            try {
+                jsonObject = new JSONObject(data);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            JSONArray cityArray = jsonObject.getJSONArray("data");
+
+            for (int i = 0; i<cityArray.length(); i++){
+                JSONObject cityObj = cityArray.getJSONObject(i);
+                String ctName = cityObj.get("State").toString();
+                if (ctName.equals(cityName)){
+                    String population = cityObj.get("Population").toString();
+                    tvShowResult.setText(population);
+                    break;
+                }else {
+                    tvShowResult.setText("not found");
+                }
+            }
+        }
     }
 }
 
